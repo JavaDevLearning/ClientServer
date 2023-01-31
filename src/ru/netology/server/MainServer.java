846872116bb014aab55e9ade9e2a6b5795feeee5
Andrey Belkin <dkvd1@yandex.ru>
2023-01-31 20:37:49 +0500

    Add try catch in MainServer

--- src/ru/netology/server/MainServer.java
+++ src/ru/netology/server/MainServer.java
@@ -8,18 +8,22 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        ServerSocket serverSocket = new ServerSocket(8081);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try (ServerSocket serverSocket = new ServerSocket(8081);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(clientSocket.getInputStream()))) {
 
-        System.out.println("New connection true");
+            System.out.println("New connection true");
 
-        final String name = in.readLine();
+            final String name = in.readLine();
 
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
 
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 }
